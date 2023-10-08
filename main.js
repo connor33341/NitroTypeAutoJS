9ed04ae2e98f5ff5d8e9618a856e2f51b956c0f2
Connor <107011324+connor33341@users.noreply.github.com>
2023-10-08 10:27:02 -0700

    Forgot about CROS

--- main.js
+++ main.js
@@ -1,15 +1,66 @@
 // connor33341, Yes I am using semi colins 
-const KeyCodeReader = "https://raw.githubusercontent.com/connor33341/NitroTypeAutoJS/main/KeyCodeReader.js";
-const WaitForElement = "https://raw.githubusercontent.com/connor33341/NitroTypeAutoJS/main/WaitForElement.js";
-let ScriptData = "";
 const DashLetter = ".dash-letter";
 const Retracting = ".is-retracting";
-var ScriptElement = document.createElement("script");
-ScriptElement.src = KeyCodeReader;
-document.head.appendChild(ScriptElement);
-var ScriptElement2 = document.createElement("script");
-ScriptElement2.src = WaitForElement;
-document.head.appendChild(ScriptElement2);
+const Endpoint = "https://raw.githubusercontent.com/connor33341/NitroTypeAutoJS/main/config/ButtonMap.txt"
+let Data = ""
+let DataArray = []
+function Setup(){
+    var XHR = new XMLHttpRequest()
+    XHR.onreadystatechange = function(){
+        if (XHR.readyState === 4 && XHR.status === 200){
+            Data = XHR.responseText
+            console.log("DATA: "+Data)
+            ProcessData()
+        }
+    }
+    XHR.open("GET",Endpoint,true)
+    XHR.send()
+}
+function ProcessData(){
+    var Lines = Data.split("\n")
+    for (let index in Lines){
+        var Line = Lines[index]
+        console.log("Processing Line#: "+String(index))
+        var LineData = Line.split(";")[0]
+        var LineString = LineData.split("'")
+        if (LineString[1] && LineString[2]){
+            var Key = LineString[1].split("'")[0]
+            if (Key == "_REMOVED"){
+                console.log("Key Removed")
+            } else {
+                var KeyCode = LineString[2].split(":")[1]
+                DataArray.push([Key,KeyCode])
+                console.log("Added KEY: "+Key+" & KEYCODE: "+KeyCode)
+            }
+        } else {
+            console.log("Error: Bugged Line")
+        }
+    }
+}
+function ClassNameWait(className) {
+    const targetElement = document.querySelector(`.${className}`);
+    if (targetElement) {
+      console.log("Element already exists");
+      return targetElement;
+    }
+    const observer = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+          const addedNode = Array.from(mutation.addedNodes).find(node => {
+            return node.classList && node.classList.contains(className);
+          });
+          
+          if (addedNode) {
+            observer.disconnect();
+            console.log("Element added");
+            return addedNode;
+          }
+        }
+      });
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+  }
+  
 function Main(){
     if (true){
         if (typeof jQuery == "undefined"){
@@ -47,8 +98,13 @@ function Main(){
         console.log("Error: SetUp Function not found");
     };
 };
-ScriptElement.onload = function(){
-    ScriptElement2.onload = function(){
-        Main();
-    }
-}
+function Load(){
+    ScriptElement.onload = function(){
+        console.log("Script 1 Loaded");
+        ScriptElement2.onload = function(){
+            console.log("Script 2 Loaded");
+            Main();
+        };
+    };
+};
+Load();
